0aff5048b6ca5a95f99970ec97ba69f0a38a67ad
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2024-04-18 21:14:09 -0500

    completing the calculator interface

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyCalculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomeCalculator());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, home: MyHomeCalculator());
   }
 }
 
@@ -25,10 +26,74 @@ class _MyHomeCalculatorState extends State<MyHomeCalculator> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My primera calculadora"),
+        title: Text("Mi primera calculadora JWCA"),
         centerTitle: true,
       ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.end),
+      body: Column(
+        children: [
+          Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.black,
+                child: Center(
+                  child: Text(
+                    '0',
+                    style: TextStyle(fontSize: 36, color: Colors.white),
+                  ),
+                ),
+              )),
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  botonConfig(['C', '+/-', '%', '÷']),
+                  botonConfig(['7', '8', '9', '×']),
+                  botonConfig(['4', '5', '6', '-']),
+                  botonConfig(['1', '2', '3', '+']),
+                  botonConfig(['0', '.', '=']),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Creando el widget boton
+  Widget botonConfig(List<String> buttons) {
+    return Expanded(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: buttons.map((text) => CustomButton(text)).toList(),
+    ));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  //const CustomButton({super.key});
+  final String text;
+
+  CustomButton(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(6.0),
+        height: double.infinity,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(new Radius.circular(16.0)),
+          color: Colors.black87,
+        ),
+        child: Text(
+          text,
+          style: new TextStyle(color: Colors.white, fontSize: 26.0),
+        ),
+      ),
     );
   }
 }
